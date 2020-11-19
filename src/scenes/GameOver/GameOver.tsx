9460a171edbae67ca983f031101472b95139ba09
David Elstob <david.elstob@vidsy.co>
2020-11-19 20:01:30 +0000

    Play Again button

--- src/scenes/GameOver/GameOver.tsx
+++ src/scenes/GameOver/GameOver.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from "styled-components";
 import Stage from "../../components/Stage";
 
 interface IProps {
@@ -8,15 +9,31 @@ interface IProps {
 const GameOver = ({ setGameState }: IProps) => {
   return (
     <Stage>
-      <button
+      High Scores TODO
+      <Button
         onClick={() =>
           setGameState((gameState: any) => ({ ...gameState, scene: "game" }))
         }
       >
         Play Again
-      </button>
+      </Button>
     </Stage>
   );
 };
+
+const Button = styled.button`
+  border: 5px outset #ccc;
+  background: #ddd;
+  color: #000;
+  cursor: pointer;
+  display: block;
+  font-family: monospace;
+  font-size: 2rem;
+  font-weight: bold;
+  letter-spacing: 2px;
+  margin: 10rem auto;
+  text-transform: uppercase;
+  padding: 1rem;
+`;
 
 export default GameOver;
